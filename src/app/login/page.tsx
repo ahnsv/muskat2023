102cdd30dd77dbd964df4c023719f359fbfa0642
ahnsv <ahnsv@bc.edu>
2023-09-06 23:09:57 +0900

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
+
+async function signInWithKakao() {
+  const supabase = createClient(
+    "https://lpvmomdvmazjofbicqnt.supabase.co",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "kakao",
+  });
+}
+
 export default function LoginPage() {
-    return (
-        <div className="container">
-            login
-        </div>
-    )
+  return (
+    <div className="flex">
+      <div className="left-side h-screen w-full bg-black flex-1"></div>
+      <div className="right-side flex-1 flex items-center justify-center flex-col">
+        <div className="login-page-logo w-24 h-24">Logo</div>
+        <div className="providers" onClick={signInWithKakao}>Providers</div>
+      </div>
+    </div>
+  );
 }
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -4,10 +4,12 @@ export default function OrderPage() {
       <h1 className="font-bold leading-7 text-gray-900 text-7xl">
         주문하기
       </h1>
+      {/* <hr className="h-px mt-16 bg-gray-200 border-0 dark:bg-gray-700" /> */}
       <form className="mt-12">
         <div className="space-y-12">
           <div className="cart">
-            <p className="title text-xl text-gray-500">상품 담기</p>
+            <p className="title text-3xl text-gray-500 font-bold">상품 담기</p>
+            <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
             <div className="product-item flex flex-row space-x-8 mt-1 align-center">
               <div className="product-name">샤인머스켓 2kg</div>
               <div className="product-price">30,000</div>
