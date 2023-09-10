1c1764775c6167374f06e4d3bae77bc951c556fc
ahnsv <ahnsv@bc.edu>
2023-09-10 16:12:48 +0900

    feat: add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 z-20 text-gray-600 w-full p-4 bg-transparent shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 ">
+      Copyrights 매목농원. All rights reserved. 사업자등록번호: 375-93-01716 |
+      대표: 안정해 경북 김천시 신음동 27-7 (매목농원) | 대표번호: 010-2994-1682
+    </footer>
+  );
+};
