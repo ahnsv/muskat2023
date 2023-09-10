ed5241d5cb1dcd30e7b04e370ce1d8af66517c37
ahnsv <ahnsv@bc.edu>
2023-09-10 18:29:29 +0900

    style: use section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,14 @@
 export const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 text-gray-600 w-full p-4 bg-transparent shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 ">
-      Copyrights 매목농원. All rights reserved. 사업자등록번호: 375-93-01716 |
-      대표: 안정해 경북 김천시 신음동 27-7 (매목농원) | 대표번호: 010-2994-1682
+    <footer className="bottom-0 left-0 z-20 text-gray-400 w-full p-4 bg-transparent shadow md:flex md:p-8 dark:bg-gray-800 flex flex-col bg-cyan-900 md:pb-16">
+      <section>
+        <div className="info">
+          <p>Copyrights 매목농원. All rights reserved.</p>
+          <p>사업자등록번호: 375-93-01716</p>
+          <p>대표: 안정해 경북 김천시 신음동 27-7 (매목농원)</p>
+          <p>대표번호: 010-2994-1682</p>
+        </div>
+      </section>
     </footer>
   );
 };
