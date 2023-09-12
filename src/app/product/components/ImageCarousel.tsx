42ef5bbd266568c1eab4bc066eb67c1965a461de
ahnsv <ahnsv@bc.edu>
2023-09-12 19:47:17 +0900

    feat: add styles

--- src/app/product/components/ImageCarousel.tsx
+++ src/app/product/components/ImageCarousel.tsx
@@ -2,8 +2,18 @@
 
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import product1 from "~/public/assets/product/product-1.jpeg";
+import product10 from "~/public/assets/product/product-10.jpeg";
+import product2 from "~/public/assets/product/product-2.jpeg";
+import product3 from "~/public/assets/product/product-3.jpeg";
+import product4 from "~/public/assets/product/product-4.jpeg";
+import product5 from "~/public/assets/product/product-5.jpeg";
+import product6 from "~/public/assets/product/product-6.jpeg";
+import product7 from "~/public/assets/product/product-7.jpeg";
+import product8 from "~/public/assets/product/product-8.jpeg";
+import product9 from "~/public/assets/product/product-9.jpeg";
 
 const ImageCarousel: React.FC = () => {
   const settings = {
@@ -12,30 +22,34 @@ const ImageCarousel: React.FC = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: false,
   };
+  const products = [
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    product7,
+    product8,
+    product9,
+    product10,
+  ];
   return (
     <div className="w-full h-64 bg-gray-200 rounded-lg shadow-md">
       {/* Implement your image carousel logic here */}
       {/* You can use a library like react-slick or build a custom carousel */}
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {products.map((product, idx) => (
+          <div
+            className="slider-item w-48 h-full"
+            key={`product${idx + 1}`}
+            style={{
+              backgroundImage: `url(${product.src})`,
+            }}
+          ></div>
+        ))}
       </Slider>
     </div>
   );
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,57 +1,10 @@
-"use client"
-// pages/ProductDetail.tsx
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import ProductDetail from "./components/ProductDetail";
+import { Database } from "~/lib/database.types";
+import { cookies } from "next/headers";
 
-import React, { useState } from 'react';
-import ImageCarousel from './components/ImageCarousel';
-
-const ProductDetail: React.FC = () => {
-  const [selectedPackage, setSelectedPackage] = useState<string>('Option A');
-  const [price, setPrice] = useState<number>(99.99);
-
-  const handlePackageChange = (newPackage: string, newPrice: number) => {
-    setSelectedPackage(newPackage);
-    setPrice(newPrice);
-  };
-
-  return (
-    <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="max-w-lg mx-auto">
-          <ImageCarousel />
-        </div>
-        <div className='px-4'>
-          <h1 className="text-2xl font-semibold mb-4">Product Name</h1>
-          <p className="text-gray-600 mb-4">Product Description Lorem ipsum dolor sit amet...</p>
-          <div className="flex items-center mb-4">
-            <span className="text-xl font-bold text-gray-800 mr-2">${price.toFixed(2)}</span>
-            <span className="text-gray-500 text-sm">(${selectedPackage})</span>
-          </div>
-          <div className="mb-4">
-            <select
-              className="border rounded-md py-2 px-3 w-full"
-              onChange={(e) => {
-                const selectedOption = e.target.value;
-                if (selectedOption === 'Option A') {
-                  handlePackageChange('Option A', 99.99);
-                } else if (selectedOption === 'Option B') {
-                  handlePackageChange('Option B', 129.99);
-                } else if (selectedOption === 'Option C') {
-                  handlePackageChange('Option C', 149.99);
-                }
-              }}
-            >
-              <option value="Option A">Option A</option>
-              <option value="Option B">Option B</option>
-              <option value="Option C">Option C</option>
-            </select>
-          </div>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 focus:outline-none">
-            Add to Cart
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ProductDetail;
+export default async function ProductPage() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data: products } = await supabase.from("products").select();
+  return <ProductDetail options={products}/>;
+}
