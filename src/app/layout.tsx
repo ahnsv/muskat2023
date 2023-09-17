50fc5475852d6d9bfd60812f2e6034bd243e85a0
ahnsv <ahnsv@bc.edu>
2023-09-17 16:47:53 +0900

    feat: add og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AuthProvider from "./components/AuthProvider";
+import bg from "~/public/assets/bg-main.png"
 
 export const metadata: Metadata = {
   title: "험프리의 샤인머스켓 2023",
@@ -25,10 +26,18 @@ export default async function RootLayout({
   const accessToken = session?.access_token || null;
   return (
     <html lang="en">
+      <meta property="og:title" content="험프리의 샤인 머스켓 | Shine Muskat 2023"></meta>
+      <meta property="og:description" content="2023 후회없는 선택 - 험프리의 샤인 머스켓" />
+      <meta property="og:url" content="/"></meta>
+      <meta property="og:image" content={bg.src}></meta>
+      <meta
+        property="description"
+        content="2023 후회없는 선택 - 험프리의 샤인 머스켓"
+      />
       <Script src="https://www.youtube.com/iframe_api" async />
       <body>
         <AuthProvider accessToken={accessToken}>{children}</AuthProvider>
-        <div id="portal"/>
+        <div id="portal" />
       </body>
     </html>
   );
