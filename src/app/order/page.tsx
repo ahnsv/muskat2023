015c21712d500c4bbbd385164ccbc82d55f43203
ahnsv <ahnsv@bc.edu>
2023-09-19 20:10:41 +0900

    refactor: 완판 1차

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "~/lib/database.types";
 import { OrderForm } from "./components/OrderForm";
+import { redirect } from "next/navigation";
 
 type Product = Database["public"]["Tables"]["products"];
 
@@ -11,13 +12,13 @@ export default async function OrderPage() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data: products } = await supabase.from("products").select().order("id");
   // 심사를 위해 임시 제거
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // if (!session) {
-  //   redirect("/login");
-  // }
+  if (!session) {
+    redirect("/");
+  }
 
   return (
     <div className="order-page lg:container lg:mx-auto py-12 lg:px-24">
--- src/app/sections/HeroSection/HeroSection.tsx
+++ src/app/sections/HeroSection/HeroSection.tsx
@@ -18,6 +18,11 @@ export const HeroSection: FC = () => {
       className={`hero h-full flex flex-col items-center justify-center`}
       ref={containerRef}
     >
+      <div className="banner bg-red-400 rounded absolute top-0 px-4 py-2 text-white font-black my-4 mx-4 transition relative animate-bounce">
+        <span>
+          ℹ️ 수량 부족으로 인해 완판되었습니다. 빠른 시일 내에 다시 찾아뵙겠습니다. (2023-09-19)
+        </span>
+      </div>
       <SoundButton youtubeVideoID={`M0U9iaH19wo`} />
       <div
         className={`hero-title text-5xl lg:text-7xl text-gray-700 ${BlackHanSans.className} text-center italic my-8`}
@@ -27,7 +32,15 @@ export const HeroSection: FC = () => {
       </div>
       <div className="hero-menus flex items-center justify-center">
         <div className="hero-menu flex-1 mt-16">
-          <Link href={`/product`} className="flex flex-col items-center">
+          <Link
+            href={`/`}
+            className="flex flex-col items-center"
+            onClick={() =>
+              alert(
+                "수량 부족으로 인해 완판되었습니다. 빠른 시일 내에 다시 찾아뵙겠습니다."
+              )
+            }
+          >
             <Image
               src={shipping.src}
               alt="shipping"
@@ -64,7 +77,7 @@ export const HeroSection: FC = () => {
             width={300}
             height={250}
             className="cursor-pointer"
-            onClick={() => alert('준비중인 기능입니다.')}
+            onClick={() => alert("준비중인 기능입니다.")}
           />
           <div className="menu-title text-2xl font-bold text-center">
             {/* 이미지 */}
